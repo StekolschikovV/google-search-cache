1314b922d552bd4b6b3e72fd51a73dff384ce81c
mk <v.a.stekolschikov@gmail.com>
2021-03-25 10:21:41 +0200

    rm test file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CommandModule } from './command/command.module';
 
 @Module({
   imports: [CommandModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
